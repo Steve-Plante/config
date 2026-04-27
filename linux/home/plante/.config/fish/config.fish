dfbe083b8b32086343cd4929d51d258c1a41de51
Steven Plante <46610727+Steve-Plante@users.noreply.github.com>
2026-04-27 16:04:43 -0400

    Update config.fish
    did some standardization.

--- linux/home/plante/.config/fish/config.fish
+++ linux/home/plante/.config/fish/config.fish
@@ -4,14 +4,13 @@ if status is-interactive
 
     alias hist="history --show-time='%Y-%m-%d %H:%M:%S'"
     alias neovim="echo 'Did you mean nvim?'"
-    alias sudo="sudo -E"
     alias sudo-rs="sudo-rs -i"
 
     # Append to PATH # (does not work under fish version 3.1)
 
-    fish_add_path --path /usr/sbin
     fish_add_path --path ~/.local/bin
     fish_add_path --path ~/.cargo/bin
+    fish_add_path --path /usr/sbin
 
     # uncomment if cargo installed via https://sh.rustup.rs
     #function fd
@@ -21,7 +20,8 @@ if status is-interactive
     # Define extra
     set -U XDG_CONFIG_HOME "$HOME/.config"
     set -gx VISUAL /usr/bin/nvim
-    set -gx EDITOR /usr/bin/nvim 
+    set -gx EDITOR /usr/bin/nvim
+    set -U LESSFILEHIST "-"
 
     echo "custom config.fish executed..."
 
